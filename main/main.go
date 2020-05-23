9c938cc942a57c59e69f623f9772201b9ce18d2f
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-05-23 16:19:29 +0530

    Added utility function GetClient and completed Service IP part of findIPs

--- main/main.go
+++ main/main.go
@@ -9,20 +9,50 @@ import (
 	"github.com/WJayesh/healthCheck/utils"
 )
 
+var namespace string = "kube-dns"
+var svcName string = "kube-dns"
+
 var pathToCfg = flag.String("path", "", "the path to the kubeconfig file")
 var podsAllowed = flag.Bool("allowPods", false, "allow creation of lightweight pods in cluster")
 
 func main() {
 	flag.Parse()
-	connectToAPIServer()
+	var client = connectToAPIServer()
+	var IPs = findIPs(client)
+	if *pathToCfg == "" {
+		//digIPs(IPs, nil)
+	}
+	if *pathToCfg != "" && *podsAllowed == true {
+		//createPod()
+		//digIPs(IPs, pod)
+	}
+	if *pathToCfg != "" && *podsAllowed == false {
+		//var serviceIPs = GetService(nil, nil, udpPort, client)
+		//digIPs(serviceIPs, nil)
+	}
 }
 
+// The user needs to have a service account
 func connectToAPIServer() *kubernetes.Clientset {
 	var client, err = utils.GetClient(*pathToCfg)
 	if client == nil {
 		logrus.Error("Client not found: ", err)
 		return nil
-	} else {
-		return client
 	}
+	logrus.Info("Client received: ", client.LegacyPrefix)
+	return client
+}
+
+// findIPs will return a map of IP addresses grouped by Service and Pods
+// These IP addresses will be used by the application when it's running inside
+// the cluster
+func findIPs(client *kubernetes.Clientset) map[string][]string {
+	var svc, err = utils.GetService(svcName, namespace, -1, client)
+	var groupedIPs map[string][]string
+	if err == nil {
+		a := make([]string, 1)
+		groupedIPs["Service IP"] = append(a, svc.Spec.ClusterIP)
+	}
+	//var pods, e = GetPods(svc, namespace, client)
+	return groupedIPs
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,12 +1,18 @@
 package utils
 
 import (
+	"errors"
+	"log"
+
 	"github.com/sirupsen/logrus"
+	v1 "k8s.io/api/core/v1"
+	mv1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+// GetClient returns a clientset
 func GetClient(pathToCfg string) (*kubernetes.Clientset, error) {
 	var config *rest.Config
 	var err error
@@ -21,4 +27,36 @@ func GetClient(pathToCfg string) (*kubernetes.Clientset, error) {
 		return nil, err
 	}
 	return kubernetes.NewForConfig(config)
+}
+
+// GetService returns the Service with the given parameters
+// If the port is not nil, we look for any service exposing the port
+// to the outside world.
+func GetService(name string, namespace string, port int32,
+	client *kubernetes.Clientset) (*v1.Service, error) {
+	if port == -1 {
+		var svc, err = client.CoreV1().Services(namespace).Get(name, mv1.GetOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if svc != nil {
+			return svc, nil
+		} else {
+			return nil, errors.New("In-Cluster service not found")
+		}
+	} else {
+		// find services that expose the given port
+		svcs, err := client.CoreV1().Services(namespace).List(mv1.ListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, svc := range svcs.Items {
+			for _, svcPort := range svc.Spec.Ports {
+				if svcPort.Port == port {
+					return &svc, nil
+				}
+			}
+		}
+		return nil, errors.New("No external services available")
+	}
 }
