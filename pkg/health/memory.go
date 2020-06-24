72896b6b01050fc37616b643d8d3c55c1c23e366
Jayesh Sharma <WJayesh@users.noreply.github.com>
2020-06-24 10:28:40 +0530

    add comments

--- pkg/health/memory.go
+++ pkg/health/memory.go
@@ -34,12 +34,19 @@ func GetMemory(name string) int64 {
 // AddMemory multiplies the existing memory limit of deployment by memFactor
 func AddMemory(memFactor int, name string) {
 
+	// If supplied memFactor is less than 1, we default to 2
 	if memFactor < 1 {
 		memFactor = 2
 	}
+
 	currMem := GetMemory(name)
+	if currMem == -1 {
+		logrus.Error("Memory limit value could not be fetched")
+	}
 	newMem := int(currMem) * memFactor
 
+	// conflict might occur if the deployment gets updated while we're trying to modify it.
+	// hence, retry on conflict is used.
 	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
 
 		result, err := dClient.Get(context.TODO(), deployment, mv1.GetOptions{})
@@ -48,6 +55,9 @@ func AddMemory(memFactor int, name string) {
 		}
 		var i = 0
 		var updateErr error
+
+		// for all containers in the deployment, increase memory limit
+		// TODO, use number of pods var instead of hardcoding
 		for i < 2 {
 			result.Spec.Template.Spec.Containers[i].Resources.Limits =
 				make(map[v1.ResourceName]resource.Quantity)
@@ -62,18 +72,21 @@ func AddMemory(memFactor int, name string) {
 	})
 
 	if retryErr != nil {
-		logrus.Error("Retry on conflict fails: ", retryErr.Error)
+		logrus.Error("Retry on conflict fails: ", retryErr)
 	}
 
 	// Sleep till all pods are running again
 	for !PodsReady(dClient) {
+		logrus.Info("Waiting for the pods to be up and running")
 		time.Sleep(500 * time.Millisecond)
 	}
 
 }
 
 // IsOutOfMemory checks the timestamp array of Pod restarts to figure out
-// if the pods are running out of memory
+// if the pods are running out of memory. If the restart times are too frequent we
+// can assume that further restarts won't be helpful and so it is a memory issue.
 func IsOutOfMemory(ts []time.Time) bool {
+
 	return false
 }
